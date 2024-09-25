6cc69a1db9e66a2db84cdefa4a7bbf9a5c4f127e
Raoni M. <48883278+raonifabian@users.noreply.github.com>
2024-09-25 20:03:04 -0300

    Conexao com banco de dados
    Adicionado novo arquivo de conexão com banco de dados que faz a conexão direta com um banco mysql hospedado na web

--- conexao.php
+++ conexao.php
@@ -1,28 +1,25 @@
 <?php 
 
-	
+$servidor = "srv1077.hstgr.io";//define a variavel de onde está hospedado o banco de dados
+$usuario = "u967032754_educatechifsp"; //confirma o usuario do banco
+$senha = "#Fj^U4YQ3d"; //confirma a senha do banco
+$db = "u967032754_educatechifsp"; //confirma qual das tabelas do banco será usada
 
-    $servidor = "localhost";//define a variavel de onde está hospedado o banco de dados
-    $usuario = "root"; //confirma o usuario do banco
-    $senha = ""; //confirma a senha do banco
-    $db = "educatech"; //confirma qual das tabelas do banco será usada
+//Criar a conexão com o banco de dados
+$conn = mysqli_connect($servidor, $usuario, $senha, $db);
 
-    //Criar a conexão com o banco de dados
-    $conn = mysqli_connect($servidor, $usuario, $senha, $db);
-    
-	//seta consultas para o padrão utf8 permitindo acentos e caracteres especiciais
-    $conn->query("SET NAMES 'utf8'");
-	$conn->query("SET character_set_connection='utf8'");
-	$conn->query("SET character_set_client=utf8");
-	$conn->query("SET character_set_results=utf8");
-    
+//seta consultas para o padrão utf8 permitindo acentos e caracteres especiciais
+$conn->query("SET NAMES 'utf8'");
+$conn->query("SET character_set_connection='utf8'");
+$conn->query("SET character_set_client=utf8");
+$conn->query("SET character_set_results=utf8");
 
-	//Teste de conexão com o banco
-	/*if(!$conn){ //testa se a variavel conexão é true or false, se for false, mostra o erro
- 		 die("Falha na conexao: " . mysqli_connect_error());
-	}else{//mostra a conexão true com o banco
-  		echo "Conexao realizada com sucesso";
-	}*/
 
+//Teste de conexão com o banco
+/*if(!$conn){ //testa se a variavel conexão é true or false, se for false, mostra o erro
+      die("Falha na conexao: " . mysqli_connect_error());
+}else{//mostra a conexão true com o banco
+      echo "Conexao realizada com sucesso";
+}*/
 
 ?>
